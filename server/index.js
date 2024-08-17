2e54300be2125280fcb01e826cb31025a73647b9
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-17 11:21:50 +0545

    mongoDb Database connected

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
