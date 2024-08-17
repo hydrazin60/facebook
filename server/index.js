07efbec43809945ea3d2d992c704be18fda2e4e2
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-17 09:46:00 +0545

    ************* reStart Project

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import Userrouter from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
-import postrouter from "./routes/user.post.routes.js";
 
 dotenv.config();
 const app = express();
@@ -11,11 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
- 
 
-
-app.use("/facebook/api/user/v1", Userrouter);
-app.use("/facebook/api/user-post/v1", postrouter);
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
