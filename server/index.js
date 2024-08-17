5d0968aef271b9e835dd60aa9b4c5ffb5b19dfbe
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-17 11:17:24 +0545

    userSchema , commentSchema , postSchema    create

--- server/index.js
+++ server/index.js
@@ -1,24 +1,11 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(cookieParser());
 
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log(`database connected on  ${process.env.MONGODB_URI}`);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
