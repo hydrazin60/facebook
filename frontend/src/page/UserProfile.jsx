b92fa5e2c72a5a45cd778229699f8cf209fe5d94
hydrazin60 <sureshkhdk45@gmail.com>
2024-10-30 12:57:29 +0545

    Follow and unflollow  API function add on frontend

--- frontend/src/page/UserProfile.jsx
+++ frontend/src/page/UserProfile.jsx
@@ -1,7 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { LogIn, Section } from "lucide-react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaChevronDown } from "react-icons/fa6";
 import { BsThreeDots } from "react-icons/bs";
 import { FaSortDown } from "react-icons/fa";
@@ -10,12 +10,16 @@ import UserPosts from "@/components/userProfile/UserPosts";
 import { useLocation, useParams } from "react-router-dom";
 import { toast } from "sonner";
 import axios from "axios";
+import { useSelector } from "react-redux";
 
 export default function userProfile() {
+  const user = useSelector((state) => state.auth.user);
   const location = useLocation();
   const profileViewUserId = useParams();
-
   const [userProfile, setUserProfile] = React.useState({});
+  const [isFollowing, setIsFollowing] = useState(
+    user?.following?.includes(userProfile?._id)
+  );
   useEffect(() => {
     const UserProfileViewFunction = async () => {
       try {
@@ -37,6 +41,31 @@ export default function userProfile() {
     };
     UserProfileViewFunction();
   }, [profileViewUserId.id]);
+
+  const handleFollow = async (id) => {
+    try {
+      const res = await axios.get(
+        `http://localhost:4000/facebook/api/v1/user/followOrUnfollow/${id}`,
+        {
+          withCredentials: true,
+        }
+      );
+
+      if (res.data.success) {
+        setIsFollowing(!isFollowing);
+        toast.success(res.data.message);
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (err) {
+      toast.error(err.response.data.message);
+      console.log(`handleFollow error: ${err.message}`);
+    }
+  };
+ 
+   
+  
+
   return (
     <section className="w-full h-full flex flex-col overflow-y-scroll left-sidebar     ">
       <div className="w-full h-[90vh]  flex flex-col items-center   ">
@@ -103,7 +132,10 @@ export default function userProfile() {
               </div>
             </div>
             <div className="flex h-full items-end gap-2 ">
-              <Button className="bg-zinc-300 text-black hover:bg-zinc-400 hover:text-black flex items-center gap-2">
+              <Button
+                className="bg-zinc-300 text-black hover:bg-zinc-400 hover:text-black flex items-center gap-2"
+                onClick={() => handleFollow(userProfile?._id)}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -113,8 +145,13 @@ export default function userProfile() {
                 >
                   <path d="M15 14c-2.33 0-7 1.17-7 3.5V20h14v-2.5c0-2.33-4.67-3.5-7-3.5zM7.15 10.6c1.06 0 1.93-.86 1.93-1.92S8.2 6.76 7.15 6.76 5.22 7.62 5.22 8.68s.87 1.92 1.93 1.92zM11.91 9.16c0 .71-.17 1.36-.44 1.93-.83.22-1.85.62-2.67 1.24-.36-.07-.71-.12-1.09-.12C4.77 12.21 2 14.46 2 16.97v.98h5.09v-.98c0-1.43 2.56-2.63 5.41-2.63.52 0 1.02.04 1.5.11.38-.54.6-1.19.6-1.91 0-1.72-1.39-3.12-3.12-3.12S8.35 7.53 8.35 9.26c0 .43.1.85.26 1.22.48-.1.98-.15 1.49-.15 1.8 0 3.5.77 4.58 2zM22 10h-2V8h-1v2h-2v1h2v2h1v-2h2v-1z" />
                 </svg>
-                <p> Add to Friends </p>
+                {isFollowing ? (
+                  <p className="text-black text-[0.9rem] font-bold">Unfollow </p>
+                ) : (
+                  <p className={`text-black  text-[0.9rem] font-bold `}>Follow </p>
+                )}
               </Button>
+
               <Button className="bg-blue-600 hover:bg-blue-700 flex items-center gap-2 ">
                 <FaFacebookMessenger className="text-white text-lg" />
                 <p>Message</p>
